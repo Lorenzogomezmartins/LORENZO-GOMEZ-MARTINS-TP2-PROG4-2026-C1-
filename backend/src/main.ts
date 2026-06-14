20c35ba480cb88f72e62ec71e311768049470888
Lorenzo Gomez Martins <lorenzogomezmartins@gmail.com>
2026-06-14 10:33:43 -0300

    Preparacion para render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,18 +7,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  /*
-    Habilitamos CORS para que Angular, que corre en localhost:4200,
-    pueda consumir este backend en localhost:3000.
-  */
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: true,
   });
 
-  /*
-    ValidationPipe hace que funcionen las validaciones de los DTOs.
-    Ejemplo: correo obligatorio, contraseña mínima, etc.
-  */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -26,11 +18,6 @@ async function bootstrap() {
     }),
   );
 
-  /*
-    Servimos públicamente la carpeta uploads.
-    Si guardamos una imagen como uploads/foto.png,
-    se podrá ver desde http://localhost:3000/uploads/foto.png
-  */
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
