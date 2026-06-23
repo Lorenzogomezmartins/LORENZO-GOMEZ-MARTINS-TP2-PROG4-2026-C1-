e30e42a768e37bcae7b9ad07ea1631796dbeda53
Lorenzo Gomez Martins <lorenzogomezmartins@gmail.com>
2026-06-23 12:27:02 -0300

    Update publicaciones.ts

--- frontend/src/app/services/publicaciones.ts
+++ frontend/src/app/services/publicaciones.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
 export class PublicacionesService {
 
   // URL base del módulo de publicaciones del backend.
-  private apiUrl = 'http://localhost:3000/publicaciones';
+private apiUrl = 'https://redsocial-backend-fy2b.onrender.com/publicaciones';
 
   // Inyección de HttpClient para realizar peticiones HTTP.
   constructor(private http: HttpClient) {}
